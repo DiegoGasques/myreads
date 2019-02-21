506fe63ce7fdf8cf03974016f60a37b7c180c05b
Diego <diegomgasques@gmail.com>
2019-02-21 10:17:38 -0300

    Add new test to BookCardAvatar

--- src/components/BookCard/BookCardAvatar/.test.js
+++ src/components/BookCard/BookCardAvatar/.test.js
@@ -2,6 +2,7 @@ import React from "react";
 import { shallow } from "enzyme";
 import BookCardAvatar from "./index";
 import toJSON from "enzyme-to-json";
+import { books } from "../../../testFixtures";
 
 it("Should match the component snapshot", () => {
   const wrapper = shallow(<BookCardAvatar />);
@@ -12,5 +13,13 @@ it("Should match the default image as background", () => {
   const wrapper = shallow(<BookCardAvatar />);
   expect(wrapper.props().style).toEqual({
     backgroundImage: `url(https://via.placeholder.com/100x170)`
+  });
+});
+
+it("Should match the src image passed as prop", () => {
+  const src = books[0].imageLinks.thumbnail;
+  const wrapper = shallow(<BookCardAvatar src={src} />);
+  expect(wrapper.props().style).toEqual({
+    backgroundImage: `url(${src})`
   });
 });
