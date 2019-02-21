a4fb8d055a3f6bc4faeea124556842fb7333958f
Diego <diegomgasques@gmail.com>
2019-02-20 22:50:30 -0300

    Add PropTypes to Card component

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,10 +1,16 @@
 import React from "react";
 import classnames from "classnames";
+import PropTypes from "prop-types";
 
 import "./styles.css";
 
 function Card({ className, children }) {
   return <div className={classnames("card", className)}>{children}</div>;
 }
+
+Card.propTypes = {
+  className: PropTypes.string,
+  children: PropTypes.node
+};
 
 export default Card;
