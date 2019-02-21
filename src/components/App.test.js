beecd961fb10a2d4da99e9e01f3721cd03b6ef3b
Diego <diegomgasques@gmail.com>
2019-02-21 18:59:43 -0300

    Add test file for the App component

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -1,16 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './App'
+import React from "react";
+import { shallow } from "enzyme";
+import toJSON from "enzyme-to-json";
 
-/** 
- This course is not designed to teach Test Driven Development. 
- Feel free to use this file to test your application, but it 
- is not required.
-**/
+import BookApp from "./app";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div')
-  ReactDOM.render(<App />, div)
-})
-
-
+it("Should match BookApp component snapshot", () => {
+  const wrapper = shallow(<BookApp />);
+  expect(toJSON(wrapper)).toMatchSnapshot();
+});
